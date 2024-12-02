ae85065ce923d8c81f4d10e374f22485c8f19819
Sean Spradlin <seanspradlin@gmail.com>
2024-12-02 13:21:27 -0600

    chore: removed redis dependency

--- src/lib/server/cache/index.ts
+++
@@ -1,4 +0,0 @@
-import Redis from 'ioredis';
-import { env } from '$env/dynamic/private';
-if (!env.REDIS_URL) throw new Error('REDIS_URL is not set');
-export const cache = new Redis(env.REDIS_URL);
